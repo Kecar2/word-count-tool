d4c7b1458574c9e2bf9cf18427b72045efb7b161
Kecar2 <kendrycarvajalsegura>
2022-04-06 10:04:18 +0200

    computed

--- app.js
+++ app.js
@@ -7,16 +7,31 @@ Vue.createApp({
         }
     },
     methods: {
-        calculate() {
-
-        this.showTextLength = this.textInput;
-
+        saludar(nombre) {
+            return 'Alo!!!!!' + nombre;
         },
+        calculate() {
+            this.showTextLength = this.textInput;
+        },
+
+    },
+    // Las computed properties NO deberían modificar el estado
+    computed: {
         setColorNumber() {
+            // una función que está calculando un estado a partir de otro
             if (this.showTextLength.length % 2 == 0) {
                 return 'blue';
             }
+
             return 'red';
+        },
+        getTextLength() {
+            return this.showTextLength.length;
+        },
+        getArrayFromText() {
+            // me crea un array con tantos elementos como carácteres tien
+            return this.showTextLength.split('')
         }
     }
+
 }).mount('#app')
